fc3b2f59069055e198d03d0a46e5f1170af62720
Your Name <you@example.com>
2024-06-23 13:28:29 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const connectToDB = require("./db");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 8000;
 
 // Connect to MongoDB
@@ -12,28 +13,34 @@ connectToDB();
 // Middleware
 app.use(express.json());
 // CORS configuration
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://667472c46a08c69e82eb4b3b--appforfood.netlify.app',`${process.env.REACT_APP_API_URL}`],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'], // Allow these headers
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://667472c46a08c69e82eb4b3b--appforfood.netlify.app",
+      `${process.env.REACT_APP_API_URL}`,
+    ],
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"], // Allow these headers
+  })
+);
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
 });
 
 // Example routes - adjust as per your actual routes
-app.use('/api/', require("./functions/createUser"));
-app.use('/api/', require("./functions/DisplayData"));
-app.use('/api/', require("./functions/OrderData"));
+app.use("/api/", require("./functions/createUser"));
+app.use("/api/", require("./functions/DisplayData"));
+app.use("/api/", require("./functions/OrderData"));
 
 // Error handling middleware (if needed)
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
